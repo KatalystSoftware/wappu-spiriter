295dfd1633b211b00800aad04daca2218af575fa
Mikael Siidorow <mikael.siidorow@aalto.fi>
2024-04-05 21:14:21 +0300

    download saved sticker to file

--- wappu_spiriter/bot.py
+++ wappu_spiriter/bot.py
@@ -1,16 +1,49 @@
 import logging
+from pathlib import Path
 
 from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
     ContextTypes,
+    MessageHandler,
     PicklePersistence,
+    filters,
 )
 
 from wappu_spiriter.settings import Settings
 
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
 logger = logging.getLogger(__name__)
+
+
+async def save_sticker_to_file(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    if not update.message or not update.message.sticker:
+        logger.error("No message in update", update, context)
+        return
+
+    sticker = update.message.sticker
+    logger.info(sticker)
+
+    sticker_file = await context.bot.get_file(sticker)
+    logger.info(sticker_file)
+    file_extension = (
+        sticker_file.file_path.split(".")[-1] if sticker_file.file_path else "webp"
+    )
+    file_name = f"{sticker_file.file_id}.{file_extension}"
+    logger.info(file_name)
+
+    sticker_dir = Path("tmp/stickers")
+    sticker_dir.mkdir(parents=True, exist_ok=True)
+    await sticker_file.download_to_drive(sticker_dir / file_name)
+
+    await update.message.reply_sticker(sticker=sticker)
 
 
 async def hello_world(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -30,5 +63,6 @@ def main() -> None:
     )
 
     app.add_handler(CommandHandler("start", hello_world))
+    app.add_handler(MessageHandler(filters.Sticker.STATIC, save_sticker_to_file))
 
     app.run_polling()
