5ad6c403bd1fc0b5008720154a85176a327ca583
Mikael Siidorow <mikael.siidorow@aalto.fi>
2024-04-05 23:49:39 +0300

    implement game joining flow

--- wappu_spiriter/bot.py
+++ wappu_spiriter/bot.py
@@ -4,7 +4,7 @@ from shutil import rmtree
 from typing import Dict, List, Tuple, TypedDict
 
 from PIL import Image
-from telegram import Update
+from telegram import Update, constants
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
@@ -93,12 +93,133 @@ async def save_sticker_to_file(
     rmtree(tmp_dir)
 
 
-async def hello_world(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def start_game(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if not update.message:
         logger.error("No message in update", update, context)
         return
 
-    await update.message.reply_text("Hello, world!")
+    if (
+        update.message.chat.type != constants.ChatType.GROUP
+        and update.message.chat.type != constants.ChatType.SUPERGROUP
+    ):
+        await update.message.reply_text("Games can only be started in group chats!")
+        return
+
+    active_game = context.bot_data.get(update.message.chat_id)
+    if active_game is None:
+        await update.message.reply_text("First create a game with /new!")
+        return
+    status_message_id, previously_joined_players, has_started = active_game
+    has_started = True
+
+    context.bot_data[update.message.chat_id] = (
+        status_message_id,
+        previously_joined_players,
+        has_started,
+    )
+
+    await context.bot.edit_message_text(
+        f"""New game created\\!
+        
+🖼️ Game started\\!
+
+👤 {len(previously_joined_players)} players joined\\!
+
+[Play game](https://t.me/{context.bot.username})""",
+        update.message.chat_id,
+        status_message_id,
+        parse_mode=constants.ParseMode.MARKDOWN_V2,
+    )
+
+
+async def new_game(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if not update.message or not update.message.from_user:
+        logger.error("No message or user in update", update, context)
+        return
+
+    if (
+        update.message.chat.type != constants.ChatType.GROUP
+        and update.message.chat.type != constants.ChatType.SUPERGROUP
+    ):
+        await update.message.reply_text("This command is only usable in group chats!")
+        return
+
+    joined_players = set([update.message.from_user.id])
+
+    status_message = await update.message.reply_text(
+        f"""New game created!
+        
+⌛ Waiting for players to /join...
+
+👤 {len(joined_players)} players joined!
+
+Commands:
+/join - join game
+/start - start game"""
+    )
+
+    has_started = False
+    active_game = (status_message.id, joined_players, has_started)
+    context.bot_data[update.message.chat_id] = active_game
+    context.bot_data[update.message.from_user.id] = update.message.chat_id
+
+    logger.info(active_game)
+    logger.info(update.message.chat_id)
+
+
+async def join_game(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if not update.message or not update.message.from_user:
+        logger.error("No message or user in update", update, context)
+        return
+
+    if (
+        update.message.chat.type != constants.ChatType.GROUP
+        and update.message.chat.type != constants.ChatType.SUPERGROUP
+    ):
+        await update.message.reply_text("This command is only usable in group chats!")
+        return
+
+    active_game = context.bot_data.get(update.message.chat_id)
+    if active_game is None:
+        await update.message.reply_text("First create a game with /new!")
+        return
+    status_message_id, previously_joined_players, has_started = active_game
+
+    if has_started:
+        await update.message.reply_text(
+            "Game has already started, wait for the next one!"
+        )
+
+    chat_admin_ids = [
+        admin.user.id for admin in await update.message.chat.get_administrators()
+    ]
+    bot_is_admin = context.bot.id in chat_admin_ids
+    if bot_is_admin:
+        await update.message.delete()
+
+    user_already_joined = update.message.from_user.id in previously_joined_players
+    if user_already_joined:
+        return
+
+    joined_players = set(
+        list(previously_joined_players) + [update.message.from_user.id]
+    )
+    context.bot_data[update.message.chat_id] = (status_message_id, joined_players)
+    context.bot_data[update.message.from_user.id] = update.message.chat_id
+
+    await context.bot.edit_message_text(
+        f"""New game created!
+        
+⌛ Waiting for players to /join...
+
+👤 {len(joined_players)} players joined!
+
+Commands:
+/join - join game
+/start - start game""",
+        update.message.chat_id,
+        status_message_id,
+    )
 
 
 def main() -> None:
@@ -109,7 +230,9 @@ def main() -> None:
         ApplicationBuilder().token(settings.bot_token).persistence(persistence).build()
     )
 
-    app.add_handler(CommandHandler("start", hello_world))
+    app.add_handler(CommandHandler("start", start_game))
+    app.add_handler(CommandHandler("new", new_game))
+    app.add_handler(CommandHandler("join", join_game))
     app.add_handler(MessageHandler(filters.Sticker.STATIC, save_sticker_to_file))
 
     app.run_polling()
