bbc013aee7c9358dda1056d1abc9c7abaaa8e0a8
Mikael Siidorow <mikael.siidorow@aalto.fi>
2024-04-07 13:38:01 +0300

    add support for scenario overlays

--- wappu_spiriter/scenario_definitions/scenario_model.py
+++ wappu_spiriter/scenario_definitions/scenario_model.py
@@ -25,9 +25,10 @@ class Slot:
 @dataclass
 class ScenarioDefinition:
     name: str
-    base_img_path: str
     base_img_dimensions: Tuple[int, int]
     slot_list: List[SlotDefinition]
+    background_img_path: str
+    foreground_img_path: str | None = None
 
     @property
     def prompts_count(self):
@@ -72,7 +73,7 @@ class Scenario:
     def compose_image(self):
         assert self.all_slots_filled()
 
-        image = Image.open(self.scenario_definition.base_img_path)
+        image = Image.open(self.scenario_definition.background_img_path)
         for slot in self.slots:
             assert slot.submitted_image is not None
             image = overlay_pil_image_on_base_image(
@@ -83,6 +84,11 @@ class Scenario:
                     (slot.position[0] + slot.size[0], slot.position[1] + slot.size[1]),
                 ),
             )
+        image = overlay_pil_image_on_base_image(
+            image,
+            Image.open(self.scenario_definition.foreground_img_path),
+            ((0, 0), self.scenario_definition.base_img_dimensions),
+        )
 
         return image
 
@@ -90,78 +96,113 @@ class Scenario:
         return deepcopy(self)
 
 
+jail_scenario = ScenarioDefinition(
+    name="The day after wappu",
+    background_img_path="image_templates/jail.png",
+    foreground_img_path="image_templates/jail_overlay.png",
+    base_img_dimensions=(3508, 2480),
+    slot_list=[
+        {
+            "position": (666, 111),
+            "size": (777, 444),
+            "prompts": ["a decoration hanging on a wall"],
+        },
+        {
+            "position": (222, 666),
+            "size": (333, 333),
+            "prompts": ["a person sitting down not feeling so good"],
+        },
+        {
+            "position": (333, 1665),
+            "size": (2220, 444),
+            "prompts": ["a person laying down"],
+        },
+        {
+            "position": (2664, 222),
+            "size": (555, 1332),
+            "prompts": ["a person standing up"],
+        },
+    ],
+)
+
+ullis_grilling_scenario = ScenarioDefinition(
+    name="grill",
+    background_img_path="image_templates/IMG_7612.PNG",
+    base_img_dimensions=(3508, 2480),
+    slot_list=[
+        {
+            "position": (666, 140),
+            "size": (222, 222),
+            "prompts": [
+                "a wappu balloon flying in the wind",
+                "an animal on a leash",
+                "a child who lost their parents",
+            ],
+        },
+        {
+            "position": (1443, 333),
+            "size": (555, 444),
+            "prompts": [
+                "a person grilling some tasty meals",
+                "a very serious guard staring at a prisoner",
+                "an animal standing very firmly",
+            ],
+        },
+        {
+            "position": (2553, 333),
+            "size": (555, 666),
+            "prompts": [
+                "a person who lost their air balloon and are chasing it",
+                "a person trying to swat a fly",
+                "one of your favourite drinks ",
+            ],
+        },
+        {
+            "position": (1665, 1665),
+            "size": (1554, 666),
+            "prompts": [
+                "a tired person sleeping on the grass",
+                "a person that is not feeling so good",
+                "an inspirational quote",
+            ],
+        },
+    ],
+)
+
+
+ullis_tree_scenario = ScenarioDefinition(
+    name="park",
+    background_img_path="image_templates/IMG_1240.PNG",
+    base_img_dimensions=(3508, 2480),
+    slot_list=[
+        {
+            "position": (2239, 2),
+            "size": (530, 561),
+            "prompts": [
+                "someone hanging",
+                "an animal climbing something",
+                "a poster",
+            ],
+        },
+        {
+            "position": (900, 1700),
+            "size": (600, 600),
+            "prompts": [
+                "a person laying down",
+                "a person sittin",
+                "a person angry",
+            ],
+        },
+        {
+            "position": (2050, 2000),
+            "size": (200, 200),
+            "prompts": ["an item", "a drink", "an animal"],
+        },
+    ],
+)
+
 scenario_definitions = [
-    ScenarioDefinition(
-        name="grill",
-        base_img_path="image_templates/IMG_7612.PNG",
-        base_img_dimensions=(3508, 2480),
-        slot_list=[
-            {
-                "position": (666, 140),
-                "size": (222, 222),
-                "prompts": [
-                    "a wappu balloon flying in the wind",
-                    "an animal on a leash",
-                    "a child who lost their parents",
-                ],
-            },
-            {
-                "position": (1443, 333),
-                "size": (555, 444),
-                "prompts": [
-                    "a person grilling some tasty meals",
-                    "a very serious guard staring at a prisoner",
-                    "an animal standing very firmly",
-                ],
-            },
-            {
-                "position": (2553, 333),
-                "size": (555, 666),
-                "prompts": [
-                    "a person who lost their air balloon and are chasing it",
-                    "a person trying to swat a fly",
-                    "one of your favourite drinks ",
-                ],
-            },
-            {
-                "position": (1665, 1665),
-                "size": (1554, 666),
-                "prompts": [
-                    "a tired person sleeping on the grass",
-                    "a person that is not feeling so good",
-                    "an inspirational quote",
-                ],
-            },
-        ],
-    ),
-    ScenarioDefinition(
-        name="park",
-        base_img_path="image_templates/IMG_1240.PNG",
-        base_img_dimensions=(3508, 2480),
-        slot_list=[
-            {
-                "position": (2239, 2),
-                "size": (530, 561),
-                "prompts": [
-                    "someone hanging",
-                    "an animal climbing something",
-                    "a poster",
-                ],
-            },
-            {
-                "position": (900, 1700),
-                "size": (600, 600),
-                "prompts": [
-                    "a person laying down",
-                    "a person sittin",
-                    "a person angry",
-                ],
-            },
-            {
-                "position": (2050, 2000),
-                "size": (200, 200),
-                "prompts": ["an item", "a drink", "an animal"],
-            },
-        ],
-    ),
+    jail_scenario,
+    ullis_grilling_scenario,
+    ullis_tree_scenario,
 ]
