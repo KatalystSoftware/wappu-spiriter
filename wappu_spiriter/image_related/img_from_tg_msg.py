e73d137e7d0cc658d1cb525b8e34435612d6a087
patrr2 <snailmail>
2024-04-07 14:38:17 +0300

    make thumbnail missing not raise Exception

--- wappu_spiriter/image_related/img_from_tg_msg.py
+++ wappu_spiriter/image_related/img_from_tg_msg.py
@@ -23,16 +23,16 @@ async def get_picture_pil_image_from_message(
 
 async def get_sticker_pil_image_from_message(
     update: Update, context: GameStateContext
-) -> Image.Image:
+) -> Image.Image | None:
     assert update.message and update.message.sticker
 
     sticker = update.message.sticker
 
     # for animated sticker, we use the thumbnail as the img
     if sticker.is_animated or sticker.is_video:
-        assert (
-            sticker.thumbnail is not None
-        ), "Animated sticker has no thumbnail to use as the img, aborting"
+        if sticker.thumbnail is None:
+            print("Animated sticker has no thumbnail to use as the img")
+            return None
 
         sticker_file = await context.bot.get_file(sticker.thumbnail)
         sticker_byte_array = await sticker_file.download_as_bytearray()
